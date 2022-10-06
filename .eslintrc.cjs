ae5f1562e59413615a6ce89cf208dbc026fbbc9e
robbe.bierebeeck4@gmail.com <oAqhGMbYF9CBf3>
2022-10-06 17:23:27 +0200

    style: format

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,8 +3,13 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   root: true,
-  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/eslint-config-prettier", "plugin:storybook/recommended"],
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-prettier",
+    "plugin:storybook/recommended",
+  ],
   parserOptions: {
-    ecmaVersion: "latest"
-  }
+    ecmaVersion: "latest",
+  },
 };
