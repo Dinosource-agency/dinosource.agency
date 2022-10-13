4e82315e68be7dacf73ecd73ad760b84e7ae6cff
robbe.bierebeeck4@gmail.com <oAqhGMbYF9CBf3>
2022-10-13 22:42:12 +0200

    feat: add dino component that follows cursor

---
+++ src/components/DinoHeadHero.vue
@@ -0,0 +1,95 @@
+<template>
+  <section class="o-dino-header-hero">
+    <div class="o-dino-header-hero__text-wrapper">
+      <p class="o-dino-header-hero__text-wrapper__text">READY FOR</p>
+      <p class="o-dino-header-hero__text-wrapper__text">IMPACT</p>
+    </div>
+    <Renderer
+      ref="rendererC"
+      antialias
+      :orbit-ctrl="{ enabled: false, enableDamping: true }"
+      :alpha="true"
+      resize="window"
+      class="renderer"
+    >
+      <Camera ref="cameraC" :far="1000000" :position="{ x: 0, y: 0, z: 30 }" />
+      <Scene ref="sceneC">
+        <DirectionalLight color="#008400" :position="{ x: 0, y: 0, z: 10 }" />
+        <AmbientLight></AmbientLight>
+        <GltfModel
+          :rotation="{ y: 5.4 }"
+          :position="{ x: 0, y: -5, z: 0 }"
+          src="models/glftfDinoHeadNoComp.glb"
+          @load="onReady"
+        />
+      </Scene>
+    </Renderer>
+  </section>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import { Camera, GltfModel, AmbientLight, Renderer, Scene } from "troisjs";
+import * as THREE from "three";
+
+const rendererC = ref();
+const sceneC = ref();
+const cameraC = ref();
+
+// texture loader
+const cubeTextureLoader = new THREE.CubeTextureLoader();
+
+// texture
+const environmentMapTexture = cubeTextureLoader.load([
+  "/environmentMaps/3/px.webp",
+  "/environmentMaps/3/nx.webp",
+  "/environmentMaps/3/py.webp",
+  "/environmentMaps/3/ny.webp",
+  "/environmentMaps/3/pz.webp",
+  "/environmentMaps/3/nz.webp",
+]);
+
+/*
+// Material
+ */
+const dinoMetalMaterial = new THREE.MeshStandardMaterial({
+  color: 0xffffff,
+  roughness: 0,
+  metalness: 1,
+  envMap: environmentMapTexture,
+});
+
+/*
+// Loading Mesh
+ */
+const onReady = (model) => {
+  model.scene.traverse((child) => {
+    if (child.isMesh) {
+      // handle both multiple and single materials
+      Array.isArray(child.material) ? child.material : [child.material];
+
+      /*
+       * Applying the texture to the material
+       */
+      child.material = dinoMetalMaterial;
+    }
+  });
+};
+onMounted(() => {
+  const renderer = rendererC.value;
+  const camera = cameraC.value;
+
+  renderer.onBeforeRender(() => {
+    document.addEventListener("mousemove", (event) => {
+      //getting the mouse position where the axis is the center of the screen
+      const mouse = {
+        x: (event.clientX / window.innerWidth) * 2 - 1,
+        y: -(event.clientY / window.innerHeight) * 2 + 1,
+      };
+
+      camera.camera.position.x = mouse.x * 20;
+      camera.camera.position.y = mouse.y * 20;
+    });
+  });
+});
+</script>
