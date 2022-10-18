8d4de9429e8f6f18000ae1ebbb931996de69773c
robbe.bierebeeck4@gmail.com <oAqhGMbYF9CBf3>
2022-10-18 14:26:59 +0200

    feat: add emit to buttons

--- src/components/PermissionModal.vue
+++ src/components/PermissionModal.vue
@@ -6,11 +6,15 @@
       track you or something.
     </p>
     <div class="o-permission-modal__buttons">
-      <button class="a-button a-button--deny" @click="deny">Cancel</button>
-      <button class="a-button a-button--allow" @click="allow">Allow</button>
+      <button class="a-button a-button--deny" @click="$emit('cancel')">
+        Cancel
+      </button>
+      <button class="a-button a-button--allow" @click="$emit('allow')">
+        Allow
+      </button>
     </div>
   </div>
 </template>
-<scrip setup>
-
-</scrip>
+<script setup>
+defineEmits(["cancel", "allow"]);
+</script>
