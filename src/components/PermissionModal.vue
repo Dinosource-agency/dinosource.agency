7c976c482a1703d7e48d46fdbdfffb2aa6a0db64
robbe.bierebeeck4@gmail.com <oAqhGMbYF9CBf3>
2022-10-18 14:09:01 +0200

    style: format

--- src/components/PermissionModal.vue
+++ src/components/PermissionModal.vue
@@ -2,7 +2,8 @@
   <div class="o-permission-modal">
     <span class="o-permission-modal__title">Orientation permission</span>
     <p class="o-permission-modal__description">
-      We need to access the orientations of your device. Don’t worry it’s not to track you or something.
+      We need to access the orientations of your device. Don’t worry it’s not to
+      track you or something.
     </p>
     <div class="o-permission-modal__buttons">
       <button class="a-button a-button--deny" @click="deny">Cancel</button>
