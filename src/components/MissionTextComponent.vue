16f656606f14a111eecda1dc6309cc860c6117fc
Ismail <elghamri.ismail@gmail.com>
2022-10-21 15:52:31 +0200

    feat: add mission text component

---
+++ src/components/MissionTextComponent.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    default: "TITLE",
+    required: true,
+  }
+});
+</script>
+<template>
+    <section class="o-mission-text-component">
+        <span class="o-mission-text-component__title">{{ title }}</span>
+    </section>
+</template>
