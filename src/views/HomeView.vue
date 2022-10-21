6e02f24904a965e3a56b08a18f6c75756f2243d2
Ismail <elghamri.ismail@gmail.com>
2022-10-21 15:55:35 +0200

    feat: add component to home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup>
 import DinoHeadHero from "./../components/DinoHeadHero.vue";
+import MissionTextComponent from "./../components/MissionTextComponent.vue";
 </script>
 
 <template>
   <main>
     <dino-head-hero></dino-head-hero>
+    <mission-text-component
+      title="WE PRAY FOR GREAT DESIGN, AMAZING DEVELOPMENT AND MEME-TECHNO"
+    ></mission-text-component>
   </main>
 </template>
