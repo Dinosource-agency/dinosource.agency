5ae8b5948d9fd408d55f4b5657833d6be94e640e
Ismail <elghamri.ismail@gmail.com>
2022-10-21 15:53:48 +0200

    style: formatting

--- src/components/MissionTextComponent.vue
+++ src/components/MissionTextComponent.vue
@@ -4,11 +4,11 @@ defineProps({
     type: String,
     default: "TITLE",
     required: true,
-  }
+  },
 });
 </script>
 <template>
-    <section class="o-mission-text-component">
-        <span class="o-mission-text-component__title">{{ title }}</span>
-    </section>
+  <section class="o-mission-text-component">
+    <span class="o-mission-text-component__title">{{ title }}</span>
+  </section>
 </template>
