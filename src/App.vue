614dc30332767f591b58acbdc78aca8452108c8a
robbe.bierebeeck4@gmail.com <oAqhGMbYF9CBf3>
2022-10-22 22:07:03 +0200

    style: linting and formatting

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { RouterView } from "vue-router";
-import NavigationComponent from "@/components/NavigationComponent.vue"
+import NavigationComponent from "@/components/NavigationComponent.vue";
 </script>
 
 <template>
