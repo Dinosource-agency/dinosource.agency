854b72b2299131c05a34dbad1a53e302522f2e60
robbe.bierebeeck4@gmail.com <oAqhGMbYF9CBf3>
2022-10-30 01:00:08 +0200

    style: formattin

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -11,9 +11,9 @@ const AboutPageMock = {
   <section class="p-about-team">
     <div class="u-layout-grid">
       <div class="p-about-team__wrapper">
-      <dynamic-heading-component heading-style="h2" heading-type="h2">{{
-        AboutPageMock.team.title
-      }}</dynamic-heading-component>
+        <dynamic-heading-component heading-style="h2" heading-type="h2">{{
+          AboutPageMock.team.title
+        }}</dynamic-heading-component>
       </div>
     </div>
   </section>
