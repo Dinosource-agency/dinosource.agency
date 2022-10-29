839900987ec31ee8b01b0319725e5e41968b2e68
Ismail <elghamri.ismail@gmail.com>
2022-10-29 21:11:28 +0200

    feat(projectShowcaseComponent): created template

---
+++ src/components/projectShowcaseComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="o-project-showcase-component">
+    <div class="u-layout-grid">
+      <div class="o-project-showcase-component__projects-wrapper">
+        <div
+          class="o-project-showcase-component__projects-wrapper__project"
+        ></div>
+        <div
+          class="o-project-showcase-component__projects-wrapper__project"
+        ></div>
+        <div
+          class="o-project-showcase-component__projects-wrapper__project"
+        ></div>
+        <div
+          class="o-project-showcase-component__projects-wrapper__project"
+        ></div>
+        <div
+          class="o-project-showcase-component__projects-wrapper__project"
+        ></div>
+      </div>
+    </div>
+    <span class="o-project-showcase-component__title">Work</span>
+  </div>
+</template>
