1c1c5d87b7f73c72814d1603447f1058b2eb8f08
Ismail <elghamri.ismail@gmail.com>
2022-10-30 18:45:20 +0100

    feat(AboutView): added pictorial component

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,19 @@
+<script setup>
+import PictorialComponent from "../components/layout/PictorialComponent.vue";
+
+const aboutMockupData = {
+  pictorial: {
+    image: "/images/jpg/groepsfoto.jpg",
+    alt: "Group photo",
+  },
+};
+</script>
 <template>
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+  <pictorial-component
+    :image="aboutMockupData.pictorial.image"
+    :alt="aboutMockupData.pictorial.alt"
+  ></pictorial-component>
 </template>
-
-<style></style>
