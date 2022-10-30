75a3869bcfac7bbe1e4f7084ddd16bb67b5676d6
robbe.bierebeeck4@gmail.com <oAqhGMbYF9CBf3>
2022-10-30 19:26:18 +0100

    feat: add team grid to about page

--- src/components/base/DynamicHeadingComponent.vue
+++ src/components/base/DynamicHeadingComponent.vue
@@ -14,19 +14,19 @@ defineProps({
   <h1 :class="headingStyle" v-if="headingType === 'h1'">
     <slot></slot>
   </h1>
-  <h2 :class="headingStyle" v-if="headingType === 'h2'">
+  <h2 :class="headingStyle" v-else-if="headingType === 'h2'">
     <slot></slot>
   </h2>
-  <h3 :class="headingStyle" v-if="headingType === 'h3'">
+  <h3 :class="headingStyle" v-else-if="headingType === 'h3'">
     <slot></slot>
   </h3>
-  <h4 :class="headingStyle" v-if="headingType === 'h4'">
+  <h4 :class="headingStyle" v-else-if="headingType === 'h4'">
     <slot></slot>
   </h4>
-  <h5 :class="headingStyle" v-if="headingType === 'h5'">
+  <h5 :class="headingStyle" v-else-if="headingType === 'h5'">
     <slot></slot>
   </h5>
-  <h6 :class="headingStyle" v-if="headingType === 'h6'">
+  <h6 :class="headingStyle" v-else-if="headingType === 'h6'">
     <slot></slot>
   </h6>
 </template>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,9 +1,88 @@
 <script setup>
 import DynamicHeadingComponent from "@/components/base/DynamicHeadingComponent.vue";
-
+import CardComponent from "@/components/layout/CardComponent.vue";
 const AboutPageMock = {
   team: {
     title: "THESE ARE THE PEOPLE YOU NEED TO KNOW",
+    members: [
+      {
+        id: 1,
+        imageSrc: "/images/team/seppe.webp",
+        imageAlt: "Picture of Seppe",
+        cardTitle: "Seppe",
+        name: ["Seppe"],
+      },
+      {
+        id: 2,
+        imageSrc: "/images/team/elien.webp",
+        imageAlt: "Picture of Elien",
+        cardTitle: "Elien",
+        name: ["Elien"],
+      },
+      {
+        id: 3,
+        imageSrc: "/images/team/ismael.webp",
+        imageAlt: "Picture of Ismael",
+        cardTitle: "Ismael",
+        name: ["Ismael"],
+      },
+      {
+        id: 4,
+        imageSrc: "/images/team/ellen.webp",
+        imageAlt: "Picture of Ellen",
+        cardTitle: "Ellen",
+        name: ["Ellen"],
+      },
+      {
+        id: 5,
+        imageSrc: "/images/team/robbe.webp",
+        imageAlt: "Picture of robbe",
+        cardTitle: "robbe",
+        name: ["Robbe"],
+      },
+      {
+        id: 6,
+        imageSrc: "/images/team/alejandro.webp",
+        imageAlt: "Picture of alejandro",
+        cardTitle: "alejandro",
+        name: ["Alejandro"],
+      },
+      {
+        id: 7,
+        imageSrc: "/images/team/lukas.webp",
+        imageAlt: "Picture of lukas",
+        cardTitle: "lukas",
+        name: ["Lukas"],
+      },
+      {
+        id: 8,
+        imageSrc: "/images/team/rickey.webp",
+        imageAlt: "Picture of rickey",
+        cardTitle: "rickey",
+        name: ["Rickey"],
+      },
+      {
+        id: 9,
+        imageSrc: "/images/team/robin.webp",
+        imageAlt: "Picture of robin",
+        cardTitle: "robin",
+        name: ["Robin"],
+      },
+      {
+        id: 10,
+        imageSrc: "/images/team/aimane.webp",
+        imageAlt: "Picture of aimane",
+        cardTitle: "aimane",
+        name: ["Aimane"],
+      },
+      {
+        id: 11,
+        imageSrc: "/images/team/sien.webp",
+        imageAlt: "Picture of sien",
+        cardTitle: "sien",
+        name: ["Sien"],
+      },
+    ],
   },
 };
 </script>
@@ -11,9 +90,23 @@ const AboutPageMock = {
   <section class="p-about-team">
     <div class="u-layout-grid">
       <div class="p-about-team__wrapper">
-        <dynamic-heading-component heading-style="h2" heading-type="h2">{{
-          AboutPageMock.team.title
-        }}</dynamic-heading-component>
+        <dynamic-heading-component
+          class="p-about-team__title"
+          heading-style="h2"
+          heading-type="h2"
+          >{{ AboutPageMock.team.title.toLocaleUpperCase() }}</dynamic-heading-component
+        >
+        <div class="p-about-team__grid">
+          <card-component
+            v-for="member in AboutPageMock.team.members"
+            :key="member.id"
+            :image-src="member.imageSrc"
+            :image-alt="member.imageAlt"
+            :card-tags="member.name"
+            class="p-about-team__grid__card"
+          >
+          </card-component>
+        </div>
       </div>
     </div>
   </section>
