9da4d567d11351a857d5585aafdb80aa50dee111
Ismail <elghamri.ismail@gmail.com>
2022-10-30 18:41:32 +0100

    feat(pictorialComponent): added picture component

---
+++ src/components/layout/PictorialComponent.vue
@@ -0,0 +1,19 @@
+<script setup>
+const props = defineProps({
+  image: {
+    type: String,
+    required: true,
+  },
+  alt: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+<template>
+  <div class="o-pictorial-component">
+    <div class="u-layout-grid">
+      <img class="o-pictorial-component__image" :src="props.image" :alt="props.alt" />
+    </div>
+  </div>
+</template>
