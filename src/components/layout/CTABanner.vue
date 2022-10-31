778e6fe3641b407db224312a8eb12817e64a492e
Ismail <elghamri.ismail@gmail.com>
2022-10-31 14:34:30 +0100

    style(ctaBanner): added styling

---
+++ src/components/layout/CTABanner.vue
@@ -0,0 +1,35 @@
+<script setup>
+import LinkComponent from "@/components/base/LinkComponent.vue";
+//get ctaBanner object from AboutPageMock
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  button: {
+    type: Object,
+    required: true,
+  },
+  buttonText: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+<template>
+  <div class="o-ctabanner-component">
+    <span class="o-ctabanner-component__title">{{ props.title }}</span>
+    <link-component
+      :title="props.button.title"
+      :to="props.button.to"
+      :link-type="props.button.linkType"
+      class="o-ctabanner-component__button"
+    >
+      <button
+        class="a-button a-button--primary o-navigation__items__item--primary"
+      >
+        {{ props.buttonText }}
+      </button>
+    </link-component>
+  </div>
+</template>
