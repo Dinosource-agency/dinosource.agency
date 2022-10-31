74aa9a6489eaf0dbf0347bb07cf79dfe09527426
robbe.bierebeeck4@gmail.com <oAqhGMbYF9CBf3>
2022-10-31 19:26:47 +0100

    style: formatting

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -40,12 +40,12 @@ const homeMockData = {
   <main>
     <dino-head-hero></dino-head-hero>
     <MissionTextComponent
-        size="big"
-        backgroundColor="green"
-        heading-style="h2"
-        heading-type="h2"
-        alignment="center"
-        title="WE PRAY FOR GREAT DESIGN, AMAZING DEVELOPMENT AND MEME-TECHNO"
+      size="big"
+      backgroundColor="green"
+      heading-style="h2"
+      heading-type="h2"
+      alignment="center"
+      title="WE PRAY FOR GREAT DESIGN, AMAZING DEVELOPMENT AND MEME-TECHNO"
     ></MissionTextComponent>
     <project-showcase-component
       :projectShowcase="homeMockData.projectShowcase"
