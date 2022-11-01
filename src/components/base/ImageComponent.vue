62c31df3a87058833d41f7cc8ad133485a23e416
robbe.bierebeeck4@gmail.com <oAqhGMbYF9CBf3>
2022-11-01 14:07:19 +0100

    feat: add imagecomponent

---
+++ src/components/base/ImageComponent.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineProps({
+  src: {
+    type: String,
+    required: true,
+  },
+  alt: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <figure>
+    <img :src="src" :alt="alt" />
+    <figcaption>
+      {{ alt }}
+    </figcaption>
+  </figure>
+</template>
---
+++ src/components/layout/ImageBannerComponent.vue
@@ -0,0 +1,37 @@
+<script setup>
+import PictorialComponent from "@/components/layout/PictorialComponent.vue";
+import DynamicHeadingComponent from "@/components/base/DynamicHeadingComponent.vue";
+
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  alt: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <section class="o-image-banner">
+    <div class="u-layout-grid">
+      <div>
+        <dynamic-heading-component heading-type="h3" heading-style="h3">{{
+          title
+        }}</dynamic-heading-component>
+        <p>{{ description }}</p>
+      </div>
+      <pictorial-component class="o-image-banner__image" :image="image" :alt="alt"></pictorial-component>
+    </div>
+  </section>
+</template>
