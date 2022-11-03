7c2162945485e3f0790bdac72bbd6112c488b6e0
Ismail <elghamri.ismail@gmail.com>
2022-11-03 13:55:09 +0100

    style(contactComponent): added styling

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,9 +1,20 @@
-<template>contact view</template>
-
-<script>
-export default {
-  name: "ContactView.vue",
+<script setup>
+import contactComponent from "@/components/contact/ContactComponent.vue";
+import pictorialComponent from "@/components/layout/PictorialComponent.vue";
+const ContactPageMock = {
+  pictorial: {
+    image: "/images/jpg/groepsfoto.jpg",
+    alt: "Group photo",
+  },
 };
 </script>
-
-<style scoped></style>
+<template>
+  <main>
+    <contact-component></contact-component>
+    <pictorial-component
+      :image="ContactPageMock.pictorial.image"
+      :alt="ContactPageMock.pictorial.alt"
+      class="pictorial-component--contact"
+    ></pictorial-component>
+  </main>
+</template>
