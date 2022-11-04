e5733affcaa57ccf5be3f18262b63f27ce8f88e3
robbe.bierebeeck4@gmail.com <oAqhGMbYF9CBf3>
2022-11-04 23:24:10 +0100

    style: formatting

--- src/mocks/index.js
+++ src/mocks/index.js
@@ -1,4 +1,4 @@
 export { WorkDetailMock } from "./work.js";
 export { contactPageMock } from "./contact.js";
 export { HomeMockData } from "./home.js";
-export { AboutPageMock} from "./about.js";
+export { AboutPageMock } from "./about.js";
